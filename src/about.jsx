7cfa66b63f37289c281bbd8a1c35a6ed188f32f2
Your Name <you@example.com>
2021-02-28 02:43:52 +0530

    second last update

--- src/about.jsx
+++ src/about.jsx
@@ -7,6 +7,8 @@ export default class About extends Component {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         <div class="about-section">
   <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<u>  About me  </u></h1>
+  
+  
   <div class="a1"><h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Greetings, A warm welcome from me </h3>&nbsp;</div>
   <div class = "a2"><p>I am currently pursuing engineering from Amravati University. My interest in Technology started from the teenage and i am passionate toward's it.
    I am enthusiastic about my work and with complete focus I am able to accompolish the task with an ease.</p></div>
@@ -24,7 +26,12 @@ export default class About extends Component {
     </div>
     </div>
   <div class = "a6"><p style = {{color:'red'}}>Weakness:-Many times I get too much involved in work, thus causing little impact on my health</p></div>
-  <div class="a7"><p><span>&#9733;</span>That's all about me !<span>&#9733;</span></p></div>
+  <div class="glowing"><p><span>&#9733;</span>That's all about me !<span>&#9733;</span></p></div>
+  <div>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <style dangerouslySetInnerHTML={{__html: "\n   body {\n      background-color: rgb(0, 0, 0);\n      font-family: cursive;\n   }\n   .glowing {\n      font-size: 50px;\n      color: #fff;\n      text-align: center;\n      -webkit-animation: glowing 1s ease-in-out infinite alternate;\n      -moz-animation: glowing 1s ease-in-out infinite alternate;\n      animation: glowing 1s ease-in-out infinite alternate;\n   }\n   @-webkit-keyframes glowing{\n      from {\n         text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #13d4cd, 0 0 40px #13d4cd, 0 0 50px #13d4cd, 0 0 60px #13d4cd, 0 0 70px #13d4cd;\n      }\n      to {\n      text-shadow: 0 0 20px #13d4cd, 0 0 30px #13d4cd, 0 0 40px #13d4cd, 0 0 50px #13d4cd, 0 0 60px #13d4cd, 0 0 70px #13d4cd, 0 0 80px #13d4cd;\n   }\n}\n" }} />
+        
+      </div>
 
 </div>
 
--- src/skills.jsx
+++ src/skills.jsx
@@ -1,10 +1,90 @@
 import React, { Component } from 'react'
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous"></link>
 export default class Skills extends Component {
   render() {
     return (
-<div className="skills">
-    <h1>Skills is working</h1>
+<div>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+       
+<div class="skills">
+  <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<u>-Motivation-</u></h1>
+     
+<div class="s1"><p>Give me any technology to work I should be able to perform my Best. </p></div>
+<div class ="s2"><p>Hands on experience in</p></div>
+<div class="arrow">
+                <span></span>
+                <span></span>
+                <span></span>
+  
+</div>
+<div class="s3">
+<button type="button" class="btn btn-primary">Angular</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-info">Reactjs</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-success">Javascript</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-danger">Html</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-warning">Css</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-light">MongoDB</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-warning">Nodejs</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 </div>
 
-    )}};
+<div class = "s4">
+
+<button type="button" class="btn btn-info">Express</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-success">SQL</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-light">PHP</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-primary">C</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-danger">C++</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-success">C#</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-light">ASP.Net</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+</div>
+
+<div class = "s5">
+
+<button type="button" class="btn btn-light">Agile</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-danger">Java</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-primary">Python</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-warning">Django</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-info">Ruby</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-primary">Laravel</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-info">R</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</div>
+
+<div class ="s6">
+
+<button type="button" class="btn btn-success">Jquery</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-danger">Hadoop</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-light">Spark</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-primary">Hive</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-warning">AWS</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-info">Azure</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-light">Machine Learning</button>
+</div>
+
+<div class="s7">
+
+<button type="button" class="btn btn-success">Git</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-light">RPA</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-primary">Devops</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-danger">Linux OS</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-primary">JSP</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-info">Servlet</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-success">Hibernate</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</div>
+
+<div class = "s8">
+
+<button type="button" class="btn btn-danger">Spring </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-info">S.MVC</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-primary">J.Swing</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-success">CodeIgniter</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<button type="button" class="btn btn-warning">Data Science</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+</div>
+
+
+</div>
+</div>
+    );
+  }
+};
